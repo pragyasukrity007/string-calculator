3aea41b46381f11bed243654738c6ade0c77d47d
pragyasukrity <pragyasukrity2712@gmail.com>
2024-11-23 16:55:40 +0530

    Handle comma-separated number

--- calculator/src/utils/stringCalculator.js
+++ calculator/src/utils/stringCalculator.js
@@ -2,6 +2,9 @@ export function add(numbers) {
   // Handle empty string
   if (!numbers) return 0;
 
-  // Parse single number
-  return parseInt(numbers);
+  //Handle Single number support
+  if (!numbers.includes(",")) return parseInt(numbers);
+
+  //Handle Multiple numbers comma-separated
+  return numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0);
 }
