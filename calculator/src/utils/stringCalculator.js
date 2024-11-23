f2dfb9fb4b2a063ae1bf5b7b827569c896a0a7a9
pragyasukrity <pragyasukrity2712@gmail.com>
2024-11-23 21:01:52 +0530

    Handle new lines between numbers

--- calculator/src/utils/stringCalculator.js
+++ calculator/src/utils/stringCalculator.js
@@ -3,8 +3,16 @@ export function add(numbers) {
   if (!numbers) return 0;
 
   //Handle Single number support
-  if (!numbers.includes(",")) return parseInt(numbers);
+  // if (!numbers.includes(",")) return parseInt(numbers);
 
   //Handle Multiple numbers comma-separated
-  return numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0);
+  // return numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0);
+
+  // Replace newlines with commas
+  const normalizedNumbers = numbers.replace(/\n/g, ",");
+
+  return normalizedNumbers
+    .split(",")
+    .reduce((sum, num) => sum + parseInt(num), 0);
+    
 }
