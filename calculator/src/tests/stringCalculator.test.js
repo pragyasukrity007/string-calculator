10dbbc39ba73cdd779bc659883a98f80b4b02005
pragyasukrity <pragyasukrity2712@gmail.com>
2024-11-23 16:56:02 +0530

    Test two comma-separated numbers

--- calculator/src/tests/stringCalculator.test.js
+++ calculator/src/tests/stringCalculator.test.js
@@ -6,10 +6,18 @@ describe("String Calculator", () => {
     expect(add("")).toBe(0);
   });
 
- // Test single number
-  it('returns the number for a single number input', () => {
-    expect(add('1')).toBe(1);
-    expect(add('5')).toBe(5);
-    expect(add('9')).toBe(9);
+  // Test single number
+  it("returns the number for a single number input", () => {
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
+    expect(add("9")).toBe(9);
   });
+
+  //Test two comma-separated numbers
+  it("adds two comma-separated numbers", () => {
+    expect(add("1,5")).toBe(6);
+    expect(add("2,3")).toBe(5);
+  });
+
+
 });
