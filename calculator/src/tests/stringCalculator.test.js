3a1af2d64964291f97db6afb897fcd4cc8f169c4
pragyasukrity <pragyasukrity2712@gmail.com>
2024-11-23 16:10:49 +0530

    Test Single Number

--- calculator/src/tests/stringCalculator.test.js
+++ calculator/src/tests/stringCalculator.test.js
@@ -6,4 +6,10 @@ describe("String Calculator", () => {
     expect(add("")).toBe(0);
   });
 
+ // Test single number
+  it('returns the number for a single number input', () => {
+    expect(add('1')).toBe(1);
+    expect(add('5')).toBe(5);
+    expect(add('9')).toBe(9);
+  });
 });
