4b438016ee5a441d383d2e32db41cdc6c5114191
pragyasukrity <pragyasukrity2712@gmail.com>
2024-11-23 23:37:02 +0530

    add new tests for handling negative numbers and comments for readability

--- calculator/src/tests/stringCalculator.test.js
+++ calculator/src/tests/stringCalculator.test.js
@@ -1,38 +1,38 @@
 import { add } from "@/utils/stringCalculator";
 
 describe("String Calculator", () => {
-  // Test empty string
+  //Step 1:  Test empty string
   it("returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
 
-  // Test single number
+  //Step 2: Test single number
   it("returns the number for a single number input", () => {
     expect(add("1")).toBe(1);
     expect(add("5")).toBe(5);
     expect(add("9")).toBe(9);
   });
 
-  //Test two comma-separated numbers
+  //Step 3: Test two comma-separated numbers
   it("adds two comma-separated numbers", () => {
     expect(add("1,5")).toBe(6);
     expect(add("2,3")).toBe(5);
   });
 
-  //Test multiple comma-separated numbers
+  //Step 4: Test multiple comma-separated numbers
   it("adds multiple comma-separated numbers", () => {
     expect(add("1,5,2,8,3")).toBe(19);
     expect(add("2,3,4,5")).toBe(14);
   });
 
-  //Test new lines between numbers
+  //Step 5: Test new lines between numbers
   it("supports new line as a delimiter", () => {
     expect(add("1\n2,3")).toBe(6);
     expect(add("1,2\n3")).toBe(6);
     expect(add("1\n2\n3")).toBe(6);
   });
 
-  //Test Custom Delimiter
+  //Step 6: Test Custom Delimiter
   it("supports custom single-character delimiters", () => {
     expect(add("//;\n1;2")).toBe(3);
     expect(add("//|\n1|2|3")).toBe(6);
@@ -62,4 +62,14 @@ describe("String Calculator", () => {
   it("handles missing numbers section with custom delimiter", () => {
     expect(add("//@\n")).toBe(0);
   });
+
+   // Step 7: Handle negative numbers
+   test('throws on single negative number', () => {
+    expect(() => add('-1,2')).toThrow('negative numbers not allowed: -1');
+  });
+
+  test('shows all negative numbers in exception', () => {
+    expect(() => add('1,-2,-3,4')).toThrow('negative numbers not allowed: -2, -3');
+  });
+  
 });
